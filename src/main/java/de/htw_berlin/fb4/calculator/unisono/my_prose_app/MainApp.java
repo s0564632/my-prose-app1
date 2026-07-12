00acf9d6c266f4dfa34f173a41bcfe33368333b0
unisono <unisono@systemli.org>
2026-07-12 20:19:04 +0200

    Feat: Complete Exercise 2 & 4 - Configure CI/CD workflows and embed license headers

--- src/main/java/de/htw_berlin/fb4/calculator/unisono/my_prose_app/MainApp.java
+++ src/main/java/de/htw_berlin/fb4/calculator/unisono/my_prose_app/MainApp.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2026 ro_bot (s0564632). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://apache.org
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
 package de.htw_berlin.fb4.calculator.unisono.my_prose_app;
 
 import de.htw_berlin.fb4.ossd.prose.ProseBuilder;
